bf7656221c88585ae66bdd6665be4efdb9a24fcf
gcbenito1-blip <gc.benito1997@gmail.com>
2026-03-05 19:56:18 +0800

    upda

--- tabs/tab3.py
+++
@@ -1,9 +0,0 @@
-# Prediction Tab
-import streamlit as st
-
-def render():
-    if not st.session_state.get("tab2_ready", False):
-        st.warning("Upload Your Data First")
-    else:
-        test = st.session_state['dataset']
-        st.dataframe(test)
--- tabs/tab4.py
+++
@@ -1,8 +0,0 @@
-# Model Tab
-
-import streamlit as st
-import matplotlib.pyplot as plt
-
-def render():
-    with st.container():
-        st.markdown("**Regression Performance**")
